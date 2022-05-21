be7692d2911b2f0800c9e1cbe4d51f0daff878ae
AlejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-05-21 00:42:54 -0300

    discovering a new world

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import "./AboutUs.css";
 
-import angular from "../../assets/techs/angular-icon-1.svg";
+//import angular from "../../assets/techs/angular-icon-1.svg";
 import aws1 from "../../assets/techs/aws-2.svg";
 import aws2 from "../../assets/techs/aws-dynamodb.svg";
 import django from "../../assets/techs/django.svg";
-import github from "../../assets/techs/github-logo.svg";
+//import github from "../../assets/techs/github-logo.svg";
 import graphql from "../../assets/techs/graphql.svg";
-import intellij from "../../assets/techs/intellij-idea-1.svg";
+//import intellij from "../../assets/techs/intellij-idea-1.svg";
 import java from "../../assets/techs/java-4.svg";
-import kotlin from "../../assets/techs/kotlin-1.svg";
+//import kotlin from "../../assets/techs/kotlin-1.svg";
 import prisma from "../../assets/techs/prisma-3.svg"
 import git from "../../assets/techs/git-icon.svg"
 import css from "../../assets/techs/css-3.svg";
@@ -18,18 +18,18 @@ import javascript from "../../assets/techs/javascript-1.svg"
 import postgresql from "../../assets/techs/postgresql.svg"
 import jquery from "../../assets/techs/jquery-4.svg";
 import sass from "../../assets/techs/sass-1.svg";
-import docker1 from "../../assets/techs/docker-3.svg"
+//import docker1 from "../../assets/techs/docker-3.svg"
 import docker2 from "../../assets/techs/docker.svg"
-import mongo from "../../assets/techs/mongodb-icon-1.svg";
+//import mongo from "../../assets/techs/mongodb-icon-1.svg";
 import mysql from "../../assets/techs/mysql-6.svg";
 import nodejs from "../../assets/techs/nodejs-icon.svg";
 import python from "../../assets/techs/python-5.svg";
 import react from "../../assets/techs/react-2.svg";
 import redux from "../../assets/techs/redux.svg";
-import spark from "../../assets/techs/spark-2.svg";
+//import spark from "../../assets/techs/spark-2.svg";
 import typescript from "../../assets/techs/typescript.svg";
-import vue from "../../assets/techs/vue-js-1.svg";
-import webstorm from "../../assets/techs/webstorm-icon.svg";
+//import vue from "../../assets/techs/vue-js-1.svg";
+//import webstorm from "../../assets/techs/webstorm-icon.svg";
 
 
 // techs missing: prisma
