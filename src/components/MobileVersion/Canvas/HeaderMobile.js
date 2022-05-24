50222d442c1c9d167aba100135022097e76e31c2
AlejoDemi <alejo.demitropulos@ing.austral.edu.ar>
2022-05-23 21:43:38 -0300

    discovering a new world

--- src/components/MobileVersion/Canvas/HeaderMobile.js
+++ src/components/MobileVersion/Canvas/HeaderMobile.js
@@ -18,7 +18,10 @@ const HeaderMobile = () => {
 
     return (
         <div className={"headerMobile"} style={{position:"relative",height:"100vh",zIndex:"1"}}>
-            <img src={logo} className={bright?"logoBright":"logo"}/>
+            <div className={"logoDiv"}>
+                <img src={logo} className={bright?"logoBright":"logo"}/>
+            </div>
+
             <div className={"slogan"}>MOVING THE WORLD</div><div style={{color:"#FFB4D8",fontStyle:"italic"}} className={"slogan"}>FORWARD.</div>
         </div>
     );
